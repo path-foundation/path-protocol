134cbe31e2178c337107ed0a787de165c0cec43d
Andrey <muzzzy@gmail.com>
2019-07-27 18:45:32 +0300

    Changing test rpc port

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,5 @@
 module.exports = {
     copyPackages: ['openzeppelin-solidity'],
-    testrpcOptions: '--mnemonic="kiwi just service vital feature rural vibrant copy pledge useless fee forum" --port 8555'
+    testrpcOptions: '--mnemonic="kiwi just service vital feature rural vibrant copy pledge useless fee forum" --port 7545'
 
 };
